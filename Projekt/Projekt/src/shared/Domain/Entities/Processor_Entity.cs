9e0e62747abb00a73909ab7f5cae38924de8f0d0
Patryk-Paw <patrykpaw06@gmail.com>
2024-10-29 22:52:38 +0100

    Add files via upload

---
+++ Projekt/Projekt/src/shared/Domain/Entities/Processor_Entity.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Domain.Entities;
+
+internal class Processor_Entity : Sprzet_Entity
+{
+    public int ProcessorId { get; set; }
+    public string? ProcessorName { get; set; }
+    public string? ProcessorSlot { get; set; }
+    public double ProcessorCores { get; set; }
+    public double ProcessorFrequency { get; set; }
+    public double ProcessorThreads { get; set; }
+}
---
+++ Projekt/Projekt/src/shared/Domain/Entities/Sprzet_Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities;
+
+internal class Sprzet_Entity
+{
+    public int ItemID { get; set; }
+    public string? ItemName { get; set; }
+    public string? ItemType { get; set; }
+    public double ItemCost { get; set; }
+    public int ItemQuantity { get; set; }
+    public bool Accesibility { get; set; } 
+}
